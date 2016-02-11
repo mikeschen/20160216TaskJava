d2f5e0f4c6da149981122278fe236020416ed023
Michael Schennum <mikeschenphoto@gmail.com>
2016-02-11 13:10:27 -0800

    adds new template files

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 
 public class PingPong {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
